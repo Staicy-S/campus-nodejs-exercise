6b80d79822bc394cbae3a22ce94b095866f0ae69
Staicy Stadtler <staicy@hotmail.de>
2023-08-31 11:21:27 +0200

    first try

--- exercise-1.js
+++ exercise-1.js
@@ -3,9 +3,12 @@ import { users } from "./data.js";
 //   `${users[0].name.title} ${users[0].name.first} ${users[0].name.last} is from ${users[0].location.country}`
 // );
 
-users.map((users) =>
-  console.log(
-    `${users.name.title} ${users.name.first} ${users.name.last} is from ${users.location.country}`
-  )
-);
-//
+// users.map((users) =>
+//   console.log(
+//     `${users.gender.title} ${users.name.first} ${users.name.last} is from ${users.location.country}`
+//   )
+// );
+
+const result = users.map;
+
+console.log(result);
--- female.js
+++ female.js
@@ -1,1 +1,5 @@
 import { users } from "./data.js";
+
+// let femaleUsers = users.filter((users.gender) => { return users.gender === "female" });
+
+console.log(femaleUsers);
