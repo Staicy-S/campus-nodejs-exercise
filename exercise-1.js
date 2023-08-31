9c112f1736a70c570656537a35bc57aa204fee5c
Staicy Stadtler <staicy@hotmail.de>
2023-08-31 10:51:51 +0200

    first exercise solved

---
+++ exercise-1.js
@@ -0,0 +1,10 @@
+import { users } from "./data.js";
+// console.log(
+//   `${users[0].name.title} ${users[0].name.first} ${users[0].name.last} is from ${users[0].location.country}`
+// );
+
+users.map((users) =>
+  console.log(
+    `${users.name.title} ${users.name.first} ${users.name.last} is from ${users.location.country}`
+  )
+);
--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 import { users } from "./data.js";
 
 console.log(users);
+
+// Create a new array with strings that contains
+// the first name, last name and country
+// ["Ralf Siewert is from Germany", "Christian Gayuoni is from Ghana"];
+
+// Create an array containing ONLY the female users.
+// Example output:
+// [
+//    {
+// gender: "female",
+// name: {title: "Mrs, first: "Charisse", last: "Kaagman"},
+// }
+//]
+
+// Create an array with male users only in the following format:
+// [
+//     "Ralf Siewert is 55 years old"
+// ]
+
+// Create a sorted array by age with Array.prototype.sort()
